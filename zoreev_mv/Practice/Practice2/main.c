1e1d2941053590cb819bebc2e15e3ae9a8b3e579
Mikhail-Zoreev <mzoreev@gmail.com>
2018-10-08 14:29:53 +0300

    uP

--- zoreev_mv/Practice/Practice2/main.c
+++ zoreev_mv/Practice/Practice2/main.c
@@ -2,7 +2,7 @@
 #include <locale.h>
 
 void main() {
-	int n, cow, bull, i;
+	int n, cow, bull, i, j, out;
 	long a;
 	setlocale(LC_ALL, "Russian");
 
@@ -15,8 +15,14 @@ void main() {
 
 	//Генерация числа
 
-	for (i = 0;i <= n;i++) {
+	for (i = 0;i < n;i++) { //Помещает в ячейку i
+		out = 0;
 
+		for (j = 0;j < i;j++) {
+
+		}
+
+		if (out == 1) i--;
 	}
 
 	//Проверка совпадений
@@ -25,12 +31,19 @@ void main() {
 		//Ввод числа
 		
 		do {
+			out = 0;
 			printf("Введите число ");
 			scanf("%l", &a);
-		} while ((a < 0) || (a < (10 * (n - 1))) || (a >= (10 * n)));
+			for (i = 1;i <= n;i++) { //Помещает в ячейку n-i
 
+			}
+		} while ((out == 0) || (a < 0) || (a < (10 * (n - 1))) || (a >= (10 * n)));
 		
 		//Поиск совпадений
+
+		bull = 0;
+		cow = 0;
+
 
 		if (bull < n) {
 			printf("Коров - %d\n Быков -%d\n", cow, bull);
