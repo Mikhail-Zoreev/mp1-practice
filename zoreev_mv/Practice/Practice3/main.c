66ab66d67e3181a459c6c01f1b6715fb1530af0b
MIkhail-Zoreev <mzoreev@gmail.com>
2019-06-14 00:06:10 +0300

    f

--- zoreev_mv/Practice/Practice3/main.c
+++ zoreev_mv/Practice/Practice3/main.c
@@ -8,22 +8,15 @@ void main() {
     srand((unsigned int) time(0));
     int mode, input, answer, i=0, dif,b1,b2;
     char out;
-
     do {
         printf("Select game mode 1 or 2\n");
         scanf("%d",&mode);
     } while ((mode < 1) || (mode > 2));
-
     //Режим 1
-
-    if (mode == 1) {
-        
+    if (mode == 1) {    
         input = rand() % 1000 + 1;
-
         while (answer != input) {
-
-            i++;
-            
+            i++;       
             while (1) {
                 printf("Input number ");
                 scanf("%d", &answer);
@@ -39,27 +32,19 @@ void main() {
             }
         }
     }
-
     //Режим 2
-
     if (mode == 2) {
-
         do {
             printf("Input number ");
             scanf("%d", &input);
         } while ((input < 1) || (input > 1000));
-
         b1 = 0; b2 = 1001;
-
         do {
-
             i++;
-
             do {
                 printf("Count %d ... then hidden ", (b2 - b1) / 2 +b1);
                 scanf("%*c%c", &out);
             } while ((out != '>') && (out != '<') && (out != '='));
-
             if (out == '>') b2 = (b2-b1) / 2 +b1 ;
             if (out == '<') b1 = (b2-b1) / 2 +b1 ;
         } while (out != '=');
